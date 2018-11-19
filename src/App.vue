409a28c56ef205981ba5394b9fdbdaa6ff047477
gamekingv <793470218@qq.com>
2018-11-19 16:20:58 +0800

    0.1

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+    <v-app dark>
+        <v-navigation-drawer width="250" stateless value="true" fixed :mini-variant="miniVariant" :clipped="clipped" v-model="drawer" app dark>
+            <feed-list></feed-list>
+        </v-navigation-drawer>
+        <v-toolbar fixed app :clipped-left="clipped" dark>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-btn icon @click.stop="miniVariant = !miniVariant">
+                <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+            </v-btn>
+            <v-btn icon @click.stop="clipped = !clipped">
+                <v-icon>web</v-icon>
+            </v-btn>
+            <v-btn icon @click.stop="fixed = !fixed">
+                <v-icon>remove</v-icon>
+            </v-btn>
+            <v-toolbar-title v-text="title"></v-toolbar-title>
+            <v-spacer></v-spacer>
+        </v-toolbar>
+        <v-content>
+            <v-container fluid>
+                <v-slide-y-transition mode="out-in">
+                    <v-layout column align-center>
+                        <img src="/public/v.png" alt="Vuetify.js" class="mb-5" />
+                        <blockquote>
+                            &#8220;First, solve the problem. Then, write the code.&#8221;
+                            <footer>
+                                <small>
+                                    <em>&mdash;John Johnson</em>
+                                </small>
+                            </footer>
+                        </blockquote>
+                    </v-layout>
+                </v-slide-y-transition>
+            </v-container>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+import FeedList from './views/FeedList/FeedList.vue';
+
+export default {
+    data: () => ({
+        clipped: false,
+        drawer: true,
+        fixed: false,
+        miniVariant: false,
+        title: 'Vuetify.js'
+    }),
+    methods: {
+    },
+    components: {
+        FeedList
+    }
+};
+</script>
---
+++ src/views/FeedList/FeedList.vue
@@ -0,0 +1,62 @@
+<template>
+    <v-list dense expand>
+        <v-list-tile>
+            <v-list-tile-action>
+                <v-icon>folder_special</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-title>全部</v-list-tile-title>
+        </v-list-tile>
+
+        <v-list-group active-class append-icon v-model="isActive" @click.stop="">
+            <v-list-tile slot="activator">
+                <v-list-tile-action @click="test">
+                    <v-btn flat icon>
+                        <v-icon v-text="isActive ? 'folder_open': 'folder'"></v-icon>
+                    </v-btn>
+                </v-list-tile-action>
+                <v-list-tile-title>User11111111111111111111111</v-list-tile-title>
+            </v-list-tile>
+
+            <v-list-tile v-for="(admin, i) in admins" :key="i">
+                <v-list-tile-action>
+                    <v-icon v-text="admin[1]"></v-icon>
+                </v-list-tile-action>
+                <v-list-tile-title v-text="admin[0]"></v-list-tile-title>
+            </v-list-tile>
+        </v-list-group>
+    </v-list>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        admins: [
+            ['Management', 'people_outline'],
+            ['Settings', 'settings']
+        ],
+        cruds: [
+            ['Create', 'add'],
+            ['Read', 'insert_drive_file'],
+            ['Update', 'update'],
+            ['Delete', 'delete']
+        ],
+        isActive: true
+    }),
+    methods: {
+        test(e) {
+            console.log(this.isActive);
+            this.isActive = !this.isActive;
+        },
+        feedSelect() {
+            console.log(123);
+        }
+    }
+};
+</script>
+<style scope>
+.list-button-mask {
+    position: absolute;
+    z-index: 1;
+    height: 40px;
+}
+</style>
