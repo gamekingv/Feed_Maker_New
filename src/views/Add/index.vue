1a1cade6dc8e28e833baec68979f23c444306c74
gamekingv <793470218@qq.com>
2018-11-29 00:11:48 +0800

    0.1

--- src/views/Add/index.vue
+++ src/views/Add/index.vue
@@ -1,49 +1,32 @@
 <template>
-    <v-stepper v-model="step">
-        <v-stepper-header>
-            <v-stepper-step :complete="step > 1" step="1">选择要添加的类型</v-stepper-step>
-            <v-divider></v-divider>
-            <v-stepper-step :complete="step > 2" step="2">Name of step 2</v-stepper-step>
-            <v-divider></v-divider>
-            <v-stepper-step step="3">Name of step 3</v-stepper-step>
-        </v-stepper-header>
-        <v-stepper-items>
-            <v-stepper-content step="1">
-                <v-form ref="form1" v-model="form1">
-                    <v-select v-model="type" :items="types" :rules="[requireRules]" label="类型" required></v-select>
-                    <v-layout>
-                        <v-spacer></v-spacer>
-                        <v-btn color="primary" :disabled="!form1" @click="step = 2">下一步</v-btn>
-                        <v-btn @click="clear('form1')">重置</v-btn>
-                    </v-layout>
-                </v-form>
-            </v-stepper-content>
-
-            <v-stepper-content step="2">
-                <v-form ref="form2" v-model="form2">
-                    <v-text-field v-model="name" :rules="[requireRules, characterRules]" :counter="10" label="Name" required></v-text-field>
-                    <v-text-field v-model="email" :rules="[emailRules, emailRules]" label="E-mail" required></v-text-field>
-                    <!-- <v-select v-model="select" :items="items" :rules="[v => !!v || 'Item is required']" label="Item" required></v-select> -->
-                    <v-checkbox v-model="checkbox" :rules="[v => !!v || 'You must agree to continue!']" label="Do you agree?" required></v-checkbox>
-                    <v-layout>
-                        <v-btn color="secondary" @click="step = 1">上一步</v-btn>
-                        <v-spacer></v-spacer>
-                        <v-btn color="primary" :disabled="!form2" @click="step = 3">下一步</v-btn>
-                        <v-btn @click="clear('form2')">重置</v-btn>
-                    </v-layout>
-                </v-form>
-            </v-stepper-content>
-
-            <v-stepper-content step="3">
-                <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
+    <v-stepper v-model="step" vertical non-linear>
+        <v-stepper-step editable step="1" :rules="[() => validates.form1]">选择要添加的类型</v-stepper-step>
+        <v-stepper-content step="1">
+            <v-form ref="form1" v-model="validates.form1" lazy-validation>
+                <v-select v-model="type" :items="types" :rules="[requireRules]" label="类型" required></v-select>
                 <v-layout>
-                    <v-btn color="secondary" @click="step = 2">上一步</v-btn>
                     <v-spacer></v-spacer>
-                    <v-btn color="primary" :disabled="!form2" @click="submit">完成</v-btn>
+                    <v-btn
+                        color="primary"
+                        :disabled="!validates.form1"
+                        @click="validate('form1', 2)"
+                    >下一步</v-btn>
+                    <v-btn @click="clear('form1')">重置</v-btn>
+                </v-layout>
+            </v-form>
+        </v-stepper-content>
+        <v-stepper-step editable step="2" :rules="[() => validates.form2]">填写基本信息</v-stepper-step>
+        <v-stepper-content step="2">
+            <v-form ref="form2" v-model="validates.form2" lazy-validation>
+                <v-text-field v-model="name" :rules="[requireRules]" label="名称" required></v-text-field>
+                <v-layout>
+                    <v-btn color="secondary" @click="step = 1">上一步</v-btn>
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary" :disabled="!complete" @click="submit">完成</v-btn>
                     <v-btn @click="clear('form2')">重置</v-btn>
                 </v-layout>
-            </v-stepper-content>
-        </v-stepper-items>
+            </v-form>
+        </v-stepper-content>
     </v-stepper>
 </template>
 
@@ -51,37 +34,40 @@
 export default {
     data: () => ({
         step: 0,
-        form1: true,
-        form2: true,
-        type: null,
-        name: '',
-        email: '',
-        characterRules: v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-        requireRules: v => !!v || '必填',
-        emailRules: v => /.+@.+/.test(v) || 'E-mail must be valid',
         types: [
             '分组',
             '订阅源',
             '自定义源',
             '自定义按钮'
         ],
-        checkbox: false
+        validates: {
+            form1: true,
+            form2: true
+        },
+        type: null,
+        name: '',
+        requireRules: v => !!v || '必填',
     }),
-
     methods: {
+        validate(name, next) {
+            if (this.$refs[name].validate())
+                this.step = next;
+        },
         submit() {
-            if (this.$refs.form.validate()) {
-                // Native form submission is not yet supported
-                //   axios.post('/api/submit', {
-                //     name: this.name,
-                //     email: this.email,
-                //     select: this.select,
-                //     checkbox: this.checkbox
-                //   })
-            }
+            Object.values(this.$refs).forEach(form => form.validate());
+            this.$nextTick(() => {
+                if (this.complete) {
+                    console.log(1);
+                }
+            });
         },
         clear(name) {
             this.$refs[name].reset();
+        }
+    },
+    computed: {
+        complete() {
+            return !Object.values(this.validates).some(validate => validate === false);
         }
     }
 };
